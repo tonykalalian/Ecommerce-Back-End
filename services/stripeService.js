1593a743c593a2804485d83970e7ca3ef61e7779
Tony Kalalian <120026287+tonykalalian@users.noreply.github.com>
2023-06-14 20:02:11 +0300

    Update stripeService.js

--- services/stripeService.js
+++ services/stripeService.js
@@ -2,14 +2,13 @@ const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 class StripeService {
   async makePayment(tokenId, amount) {
-    try {
+  
       const payment = await stripe.charges.create({
         source: tokenId,
         amount,
         currency: "usd",
       });
       return payment;
-    } 
   }
 }
 
